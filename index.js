a4e9c3b6abee6fd2d027fa2428560335251560be
ErikGazovic <155158251+ErikGazovic@users.noreply.github.com>
2024-06-25 15:31:20 +0200

    Add files via upload

--- index.js
+++ index.js
@@ -1,20 +1,18 @@
-if (window.performance.navigation.type === 1) {
-    rollDice();
-  }
-   
-  function rollDice() {
-    var randomNumber1 = `images/dice${Math.floor(Math.random() * 6) + 1}.png`;
-    var randomNumber2 = `images/dice${Math.floor(Math.random() * 6) + 1}.png`;
-   
-    document.querySelector(".img1").setAttribute("src", randomNumber1);
-    document.querySelector(".img2").setAttribute("src", randomNumber2);
-  }
 
+var randomNumber1 = Math.floor(Math.random() * 6) + 1;
+var randomImageSource = "images/dice" + randomNumber1 + ".png";
+var image1 = document.querySelectorAll("img")[0];
+image1.setAttribute("src", randomImageSource);
 
-if (num1 === num2) {
-    document.querySelector("h1").innerHTML = "Draw!";
-} else if (num1 > num2) {
-    document.querySelector("h1").innerHTML = "Player 1 Wins! 🚩";
-} else {
-    document.querySelector("h1").innerHTML = "Player 2 Wins! 🚩";
+var randomNumber2 = Math.floor(Math.random() * 6) + 1;
+var randomImageSource2 = "images/dice" + randomNumber2 + ".png";
+document.querySelectorAll("img")[1].setAttribute("src", randomImageSource2);
+if (randomNumber1 > randomNumber2) {
+  document.querySelector("h1").innerHTML = "🚩 Play 1 Wins!";
+}
+else if (randomNumber2 > randomNumber1) {
+  document.querySelector("h1").innerHTML = "Player 2 Wins! 🚩";
+}
+else {
+  document.querySelector("h1").innerHTML = "Draw!";
 }
